55c881978dc38c4cc878abfe8275411ff2c12dc1
Arnab Mitra <arnabmitra471@gmail.com>
2024-09-15 15:08:22 +0530

    Fixed the division problem in temperature conversion

--- c_to_f.c
+++ c_to_f.c
@@ -4,10 +4,10 @@ int main(){
     int celsius;
     float fahrenheit;
     
-    printf("Enter the temperature in celsius");
+    printf("Enter the temperature in celsius: ");
     scanf("%d",&celsius);
     
-    fahrenheit  = (celsius * 9/5) + 32;
-    printf("%d degree celsius is %d degree fahrenheit",celsius,fahrenheit);
+    fahrenheit  = (celsius * 9.0/5.0) + 32;
+    printf("%d degree celsius is %f degree fahrenheit",celsius,fahrenheit);
     return 0;
 }
