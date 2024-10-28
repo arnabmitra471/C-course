3a6c3258711fd183bd7221ecc825a5ec9903de90
Arnab Mitra <arnabmitra471@gmail.com>
2024-10-28 22:48:58 +0530

    Initializing an array

---
+++ array_init_default.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    int marks[20] = {20,11,12},i;
+    
+    // for(i=0;i<20;i++)
+    // {
+    //     marks[i] = -1;
+    // }
+
+    for(i=0;i<20;i++)
+    {
+        printf("%d\n",marks[i]);
+    }
+    return 0;
+}
